a27205a17337b514cdddf84389b93ddb087acf71
Manish Vasu <manishvasu@Manishs-MacBook-Air.local>
2023-07-07 23:37:33 +0530

    Added linear regression module

---
+++ linear_regression.py
@@ -0,0 +1,15 @@
+import numpy
+import pandas
+
+
+class LinearRegression(object):
+    def __init__(self) -> None:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
